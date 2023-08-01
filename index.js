6ec6ccd40cb2a8e9f2e0af0b84ecd51ec9c3ed16
FlorinCamarut1 <95957482+FlorinCamarut1@users.noreply.github.com>
2023-08-01 15:03:41 +0300

    i added logic for adding and verifying users , for register i check if input is empty or already in db and for log in it checks if it exists in database and it throws specific errors wich are handled visually to my front-end

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const con = require("./src/utils/dbConnection");
-const home = require("./src/routes/homerouter");
-const api = require("./src/routes/apirouter");
-const user = require("./src/routes/userrouter");
+// const home = require("./src/routes/homerouter");
+// const api = require("./src/routes/apirouter");
+const user = require("./src/routes/UserRouter");
 const app = express();
+app.use(cors({ origin: "http://localhost:3006", optionsSuccessStatus: 200 }));
 
 const PORT = process.env.PORT || process.env.APP_PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(home);
-app.use(api);
-app.use(user);
+// app.use(home);
+// app.use(api);
+app.use("/user", user);
 
 app.listen(PORT);
 console.log("Server started at http://localhost:" + PORT);
--- src/routes/userrouter.js
+++ src/routes/userrouter.js
@@ -1,13 +1,21 @@
 const { Router } = require("express");
-const app = Router();
-app.get("/user", (req, res) => {
-  const user_id = req.query.id;
-  const token = req.query.token;
-  const geo = req.query.geo;
-  res.send({
-    user_id: user_id,
-    token: token,
-    geo: geo,
-  });
-});
-module.exports = app;
+const router = Router();
+const UserController = require("../controllers/UserController");
+
+router.get("/", UserController.getUsers);
+router.post("/login", UserController.verifyUser);
+router.post("/adduser", UserController.addUser);
+router.put("/", UserController.updateUser);
+router.delete("/:iduser", UserController.deleteUser);
+
+// app.get("/user", (req, res) => {
+//   const user_id = req.query.id;
+//   const token = req.query.token;
+//   const geo = req.query.geo;
+//   res.send({
+//     user_id: user_id,
+//     token: token,
+//     geo: geo,
+//   });
+// });
+module.exports = router;
